6af0ff737439111aacbd2b5454dd5b84010f8866
GustavoC3J <gustavo.cortes@uva.es>
2025-07-10 13:50:51 +0200

    TFT working when static or future inputs are not provided

--- runners/model_builder/models/keras/tft/tft.py
+++ runners/model_builder/models/keras/tft/tft.py
@@ -13,35 +13,50 @@ class TFT(keras.Model):
     def __init__(
         self,
         hidden_units,
-        num_static_inputs,
         num_historic_inputs,
-        num_future_inputs,
         embedding_dim,
         output_size,
         num_attention_heads,
         dropout_rate=0.0,
+        num_static_inputs=None, # If it's None, static inputs will not be used.
+        num_future_inputs=None, # If it's None, future inputs will not be used.
+        prediction_window=None, # If future inputs are provided, this will be set to the number of time steps in the future inputs.
         **kwargs
     ):
         super().__init__(**kwargs)
         self.hidden_units = hidden_units
+        self.num_static_inputs = num_static_inputs
+        self.num_historic_inputs = num_historic_inputs
+        self.num_future_inputs = num_future_inputs
+        self.prediction_window = prediction_window
         self.output_size = output_size
         self.dropout_rate = dropout_rate
 
-        # Static covariate encoder
-        self.static_encoder = StaticCovariateEncoder(
-            hidden_dim=hidden_units,
-            dropout_rate=dropout_rate
-        )
 
-        # Variable selection networks
-        self.static_vsn = VariableSelectionNetwork(
-            hidden_units=hidden_units,
-            num_inputs=num_static_inputs,
-            embedding_dim=embedding_dim,
-            dropout_rate=dropout_rate,
-            time_distributed=False
-        )
+        if num_future_inputs is None and self.prediction_window is None:
+            raise ValueError('"num_future_inputs" or "prediction_window" is required.')
 
+        # Static
+        if num_static_inputs is None:
+            self.static_encoder = None
+            self.static_vsn = None
+        else:
+            # Static covariate encoder
+            self.static_encoder = StaticCovariateEncoder(
+                hidden_dim=hidden_units,
+                dropout_rate=dropout_rate
+            )
+
+            # Variable selection network
+            self.static_vsn = VariableSelectionNetwork(
+                hidden_units=hidden_units,
+                num_inputs=num_static_inputs,
+                embedding_dim=embedding_dim,
+                dropout_rate=dropout_rate,
+                time_distributed=False
+            )
+
+        # Historic variable selection network
         self.historic_vsn = VariableSelectionNetwork(
             hidden_units=hidden_units,
             num_inputs=num_historic_inputs,
@@ -50,13 +65,17 @@ class TFT(keras.Model):
             time_distributed=True
         )
 
-        self.future_vsn = VariableSelectionNetwork(
-            hidden_units=hidden_units,
-            num_inputs=num_future_inputs,
-            embedding_dim=embedding_dim,
-            dropout_rate=dropout_rate,
-            time_distributed=True
-        )
+        # Future variable selection network
+        if num_future_inputs is None:
+            self.future_vsn = None
+        else:
+            self.future_vsn = VariableSelectionNetwork(
+                hidden_units=hidden_units,
+                num_inputs=num_future_inputs,
+                embedding_dim=embedding_dim,
+                dropout_rate=dropout_rate,
+                time_distributed=True
+            )
 
         # LSTM history and future
         self.encoder_lstm = layers.LSTM(
@@ -133,20 +152,43 @@ class TFT(keras.Model):
             - 'historic': (batch, time_steps, num_historic_vars, emb_dim)
             - 'future': (batch, time_steps, num_future_vars, emb_dim)
         """
-        static_emb = inputs['static']
-        historic_emb = inputs['historic']
-        future_emb = inputs['future']
+        static_emb = inputs.get('static', None)
+        historic_emb = inputs.get('historic', None)
+        future_emb = inputs.get('future', None)
+
+        if historic_emb is None:
+            raise ValueError('Input "historic" is required.')
+        
+        if self.num_static_inputs is not None and static_emb is None:
+            raise ValueError('Input "static" is required when "num_static_inputs" is not None.')
+        
+        if self.num_future_inputs is not None and future_emb is None:
+            raise ValueError('Input "future" is required when "num_future_inputs" is not None.')
+        
 
         # 1. Static covariate encoding
-        static_selected = self.static_vsn(static_emb)
-        static_context = self.static_encoder(static_selected)
-        context_var_sel = static_context["context_variable_selection"]
-        context_enrichment = static_context["context_enrichment"]
-        context_state_h, context_state_c = static_context["context_state"]
+        if self.num_static_inputs is not None and static_emb is not None:
+            static_selected = self.static_vsn(static_emb)
+            static_context = self.static_encoder(static_selected)
+
+            context_var_sel = static_context["context_variable_selection"]
+            context_enrichment = static_context["context_enrichment"]
+            context_state_h, context_state_c = static_context["context_state"]
+        else:
+            # If there are no static inputs, use zeros for context
+            context_var_sel = None
+            context_enrichment = context_state_h = context_state_c = ops.zeros((ops.shape(historic_emb)[0], self.hidden_units))
+
 
         # 2. Variable selection
         historic_features = self.historic_vsn(historic_emb, context=context_var_sel)
-        future_features = self.future_vsn(future_emb, context=context_var_sel)
+
+        if self.num_future_inputs is not None and future_emb is not None:
+            self.prediction_window = ops.shape(future_emb)[1]
+            future_features = self.future_vsn(future_emb, context=context_var_sel)
+        else:
+            future_features = ops.zeros((ops.shape(historic_emb)[0], self.prediction_window, self.hidden_units))
+
 
         # 3. LSTM encoder/decoder
         encoder_output, state_h, state_c = self.encoder_lstm(
@@ -193,7 +235,6 @@ class TFT(keras.Model):
         output = self.output_layer(transformer_layer)
 
         # Keep only the prediction window
-        prediction_window = future_emb.shape[1]
-        output = output[:, -prediction_window:, :]
+        output = output[:, -self.prediction_window:, :]
 
         return output
