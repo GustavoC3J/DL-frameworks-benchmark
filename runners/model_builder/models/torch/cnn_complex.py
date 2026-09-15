Softmax removed from torch classification models, and input permute fixed

runners/model_builder/models/torch/cnn_complex.py
@@ -72,13 +72,12 @@ class CNNComplex(nn.Module):
         layers.append(nn.Flatten()) # shape [batch_size, channels]
 
         layers.append(nn.Linear(in_channels, 10))
-        layers.append(nn.Softmax(dim=1))
 
         self.model = nn.Sequential(*layers)
 
 
     def forward(self, x):
         # Switch to (batch_size, channels, height, width)
-        x = x.permute(0, 3, 2, 1)
+        x = x.permute(0, 3, 1, 2)
 
         return self.model(x)

runners/model_builder/models/torch/cnn_simple.py
@@ -29,12 +29,11 @@ class CNNSimple(nn.Module):
             nn.Dropout(dropout),
 
             # Output layer
-            nn.Linear(128, 10),
-            nn.Softmax(dim=1)
+            nn.Linear(128, 10)
         )
 
     def forward(self, x):
         # Switch to (batch_size, channels, height, width)
-        x = x.permute(0, 3, 2, 1)
+        x = x.permute(0, 3, 1, 2)
 
         return self.model(x)

runners/model_builder/models/torch/mlp_simple.py
@@ -15,8 +15,7 @@ class MLPSimple(nn.Module):
             activation,
             nn.Dropout(dropout),
 
-            nn.Linear(128, 10),
-            nn.Softmax(dim=1)
+            nn.Linear(128, 10)
         )
 
     def forward(self, x):